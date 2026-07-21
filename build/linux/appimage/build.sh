a26fca01b723d7c6826802722a2d8dea40f20c88
ZaidAlamgir <zaidkhan137782@gmail.com>
2026-07-21 10:49:24 +0530

    fix: use BINARY_NAME for AppImage icon matching to resolve file-not-found error

--- build/linux/appimage/build.sh
+++ build/linux/appimage/build.sh
@@ -30,16 +30,13 @@ if [[ "${VSCODE_ARCH}" == "x64" ]]; then
   # remove check so build in docker can succeed
   sed -i 's/grep docker/# grep docker/' pkg2appimage.AppDir/usr/share/pkg2appimage/functions.sh
 
-  APP_NAME_LC="$( echo "${APP_NAME}" | awk '{print tolower($0)}' )"
+  sed -i "s|@@BINARY_NAME@@|${BINARY_NAME}|g" recipe.yml
+  sed -i "s|@@ICON@@|${BINARY_NAME}|g" recipe.yml
 
   if [[ "${VSCODE_QUALITY}" == "insider" ]]; then
     sed -i "s|@@APP_NAME@@|${APP_NAME}-Insiders|g" recipe.yml
-    sed -i "s|@@BINARY_NAME@@|${BINARY_NAME}|g" recipe.yml
-    sed -i "s|@@ICON@@|${APP_NAME_LC}-insiders|g" recipe.yml
   else
     sed -i "s|@@APP_NAME@@|${APP_NAME}|g" recipe.yml
-    sed -i "s|@@BINARY_NAME@@|${BINARY_NAME}|g" recipe.yml
-    sed -i "s|@@ICON@@|${APP_NAME_LC}|g" recipe.yml
   fi
 
   # workaround that enforces x86 ARCH for pkg2appimage having /__w/vscodium/vscodium/build/linux/appimage/VSCodium/VSCodium.AppDir/usr/share/codium/resources/app/node_modules/rc/index.js is of architecture armhf
